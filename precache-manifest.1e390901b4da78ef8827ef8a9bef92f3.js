3791865427038987914ff9814fbb35e81b6d517c
unknown <tpmalakkad@gmail.com>
2020-08-07 22:22:53 +0530

    Updates

---
+++ precache-manifest.1e390901b4da78ef8827ef8a9bef92f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852f83a4bb416958493af08ca04100ef",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "2717f82677051d37ee3e",
+    "url": "/breakingbad/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "27d0a202c09ba6b54965",
+    "url": "/breakingbad/static/css/main.055f892c.chunk.css"
+  },
+  {
+    "revision": "2717f82677051d37ee3e",
+    "url": "/breakingbad/static/js/2.dcf80aad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbad/static/js/2.dcf80aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d0a202c09ba6b54965",
+    "url": "/breakingbad/static/js/main.3a056acf.chunk.js"
+  },
+  {
+    "revision": "ea2d3b5d3b339cb26b3a",
+    "url": "/breakingbad/static/js/runtime-main.e09d8a2d.js"
+  }
+]);
